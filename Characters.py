4d20302d3f9136127b616b8501cf740bf1940b87
TheHiddenError <148292099+TheHiddenError@users.noreply.github.com>
2023-11-13 22:05:30 -0600

    Updated Characters.py
    Added check collision, hit animation and death. The collision still needs work because it has a weird hit box.

--- Characters.py
+++ Characters.py
@@ -33,6 +33,12 @@ class Character(pygame.sprite.Sprite):
         self.y_vel = 0
         self.jumpHeight = 0
 
+        #For attack
+        self.health = 100
+        self.hit = False
+        self.stunned_cooldown = 0
+        self.Alive = True
+
         #To determine the current action of player
         self.running = False
         self.jumping = False
@@ -67,47 +73,36 @@ class Character(pygame.sprite.Sprite):
                 self.character[image.replace(".png", "")] = sprites
 
 
-
-    
-        # These functions would be for the enemies so they can move. Work in progress
-
-    # def move_left(self):
-    #     self.x_vel = -self.vel
-    #     if self.direction != "left":
-    #         self.direction = "left"
-    #         self.frame = 0
-
-    #     def move_right(self):
-    #         self.x_vel = self.vel
-    #         if self.direction != "right":
-    #             self.direction = "right"
-    #             self.frame = 0
-
-    #     def move(self,moveX, moveY):
-    #         self.rect.x += moveX
-    #         self.rect.y += moveY
-    
-
-
-
     def jump(self):
-        self.jumping = True
+        if self.stunned_cooldown == 0:
+            self.jumping = True
 
-    def attack(self):
-        if self.attack_cooldown == 0:   #This if statement prevents the player from spamming
+    def attack(self, enemy_group):
+        if self.attack_cooldown == 0 and self.stunned_cooldown == 0:   #This if statement prevents the player from spamming
             self.attack_cooldown = 50
             self.attacking = True
+            self.check_collision(enemy_group)
 
     def update_sprite(self):        #this function creates the animation
-
-        if self.running:        #this changes the animation to make it more smooth.
-            self.currentSprite = self.change_animation(self.currentSprite, "Run")
-        elif self.jumping:
-            self.currentSprite = self.change_animation(self.currentSprite, "Jump")
-        elif self.attacking:
-            self.currentSprite = self.change_animation(self.currentSprite, "Attack")
+        if self.health == 0:
+            self.currentSprite = "Death"
         else:
-            self.currentSprite = self.change_animation(self.currentSprite, "Idle")
+
+            if self.stunned_cooldown > 0:
+                self.attacking = False
+                self.jumping = False
+                self.running = False
+
+            if self.running:        #this changes the animation to make it more smooth.
+                self.currentSprite = self.change_animation(self.currentSprite, "Run")
+            elif self.jumping:
+                self.currentSprite = self.change_animation(self.currentSprite, "Jump")
+            elif self.attacking:
+                self.currentSprite = self.change_animation(self.currentSprite, "Attack")
+            elif self.hit:
+                self.currentSprite = self.change_animation(self.currentSprite, "Hit")
+            else:
+                self.currentSprite = self.change_animation(self.currentSprite, "Idle")
 
 
         image_sheet_name = "_" + self.currentSprite + "_" + self.direction
@@ -122,6 +117,8 @@ class Character(pygame.sprite.Sprite):
             self.attacking = False
             
         if self.frame >= len(self.character[image_sheet_name]):     #once the frame goes through the last frame of the current action spritesheet, it would start again in the beginning. This to prevent the frame from going out of bounds 
+            if self.health == 0:
+                self.Alive = False
             self.frame = 0
 
         self.sprite = sprites[self.frame]
@@ -134,6 +131,10 @@ class Character(pygame.sprite.Sprite):
 
         if current == desired:
             return current
+        
+        elif self.stunned_cooldown != 0:
+            current = "Hit"
+            self.frame = 0
 
         elif desired == "Jump":
             current = "Jump"
@@ -154,18 +155,34 @@ class Character(pygame.sprite.Sprite):
         return current
 
     def loop(self):
-        if self.attack_cooldown > 0:
-            self.attack_cooldown -=1  #to handle the attack_cooldown 
-        self.update_sprite()    #to constantly have animation
+        if self.Alive:
+            if self.attack_cooldown > 0:
+                self.attack_cooldown -=1  #to handle the attack_cooldown 
+
+            if self.stunned_cooldown > 0:
+                self.stunned_cooldown -=1
+            elif self.stunned_cooldown == 0:
+                self.hit = False
+            self.update_sprite()    #to constantly have animation
 
 
 
     def draw(self, window):     #draws the sprite on screen
-        window.blit(self.sprite, (self.rect.x, self.rect.y ))
+        if self.Alive:
+            window.blit(self.sprite, (self.rect.x, self.rect.y ))
 
     def check_collision(self, enemy_group):
-        if pygame.sprite.spritecollide(self.sprite, enemy_group, False, pygame.sprite.collide_mask):
-            print("It works")
+        # self.hit = False
+        hits =  pygame.sprite.spritecollide(self, enemy_group, False, pygame.sprite.collide_mask)
+        for element in hits:
+            if element.health  != 0:
+                element.health -= 10
+                element.stunned_cooldown = 15
+                element.hit = True
+                print(f"Health: {element.health}")
+            else:
+                pass
+
 
 
 class mainCharacter(Character):
@@ -177,54 +194,55 @@ class mainCharacter(Character):
     def draw(self, window):
         super().draw(window)
 
-    def handle_attack(self):
+    def handle_attack(self, enemy_group):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
-            super().attack()
+            super().attack(enemy_group)
 
     def theMove(self):      #this function handles the input that allows the character to move and jump
         self.x_vel = 0
         self.y_vel = 0 
 
-        keys = pygame.key.get_pressed()
+        if self.stunned_cooldown == 0 and self.Alive:
+            keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_LEFT]:
-            if self.jumping == False:
-                self.running = True
-            self.x_vel = -self.vel
-            if self.direction != "left":
-                self.direction = "left"
-                self.frame = 0
-        elif keys[pygame.K_RIGHT]:
-            if self.jumping == False:
-                self.running = True
-            self.x_vel = self.vel
-            if self.direction != "right":
-                self.direction = "right"
-                self.frame = 0
+            if keys[pygame.K_LEFT]:
+                if self.jumping == False:
+                    self.running = True
+                self.x_vel = -self.vel
+                if self.direction != "left":
+                    self.direction = "left"
+                    self.frame = 0
+            elif keys[pygame.K_RIGHT]:
+                if self.jumping == False:
+                    self.running = True
+                self.x_vel = self.vel
+                if self.direction != "right":
+                    self.direction = "right"
+                    self.frame = 0
 
-        elif keys[pygame.K_SPACE] and self.jumping == False:
-            self.jumpHeight = -self.vel * 3
-            self.jumping = True
-        
-        self.jumpHeight += self.GRAVITY
-        self.y_vel += self.jumpHeight
+            elif keys[pygame.K_SPACE] and self.jumping == False:
+                self.jumpHeight = -self.vel * 3
+                self.jumping = True
+            
+            self.jumpHeight += self.GRAVITY
+            self.y_vel += self.jumpHeight
 
-        if self.rect.left + self.x_vel < 0:     #this is to prevent the player from going out of the screen
-            self.x_vel -= self.rect.left 
-        elif self.rect.right + self.x_vel > 800:
-            self.x_vel = 800 - self.rect.right 
+            if self.rect.left + self.x_vel < 0:     #this is to prevent the player from going out of the screen
+                self.x_vel -= self.rect.left 
+            elif self.rect.right + self.x_vel > 800:
+                self.x_vel = 800 - self.rect.right 
 
-        if self.x_vel == 0: 
-            self.running = False
+            if self.x_vel == 0: 
+                self.running = False
 
-        if self.rect.bottom + self.y_vel > 525:     #to prevent the player from going through the map
-            self.jumping = False
-            self.jumpHeight = 0
-            self.y_vel = 525 - self.rect.bottom
+            if self.rect.bottom + self.y_vel > 525:     #to prevent the player from going through the map
+                self.jumping = False
+                self.jumpHeight = 0
+                self.y_vel = 525 - self.rect.bottom
 
-        self.rect.x += self.x_vel
-        self.rect.y += self.y_vel
+            self.rect.x += self.x_vel
+            self.rect.y += self.y_vel
 
     def check_collision(self, enemy_group):
         super().check_collision(enemy_group)
@@ -245,67 +263,36 @@ class EnemyKnight(Character):
         super().draw(window)
 
 
-    def move(self):
-        if self.move_cooldown == 0 and self.move_direction == "Left":
-            if self.rect.centerx > 250:
-                self.running = True
-                self.move_left()
-            elif self.rect.centerx < 250:
-                self.running = False
-                self.x_vel = 0
-                self.move_cooldown = 100
-                self.move_direction = "Right"
-        if self.move_cooldown == 0 and self.move_direction == "Right":
-            if self.rect.centerx < 600:
-                self.running = True
-                self.move_right()
-            elif self.rect.centerx > 600:
-                self.running = False
-                self.x_vel = 0
-                self.move_cooldown = 100
-                self.move_direction = "Left"
-
-
-        self.rect.x += self.x_vel
-
-    def move_left(self):
-        self.x_vel = -self.vel 
-        if self.direction != "left":
-            self.direction = "left"
-            self.frame = 0
-
-    def move_right(self):
-        self.x_vel = self.vel
-        if self.direction != "right":
-            self.direction = "right"
-            self.frame = 0
-
 
     def loop(self):
         super().loop()
         if self.move_cooldown > 0:
             self.move_cooldown -= 1
 
-    def detect(self):       #stack overflow. Question: How to target something in pygame
-        self.running = True
-        if self.rect.x > self.mainC.rect.x and self.direction == "right":
-            self.direction = "left"
-        elif self.rect.x < self.mainC.rect.x and self.direction == "left":
-            self.direction = "right"
-        self.x_vel = self.mainC.rect.x - self.rect.x
-        dist = math.hypot(self.x_vel)
-        if dist > 75:
-            self.x_vel = self.x_vel / dist 
-            self.rect.x += self.x_vel 
-        else:
-            self.running = False
-            self.attack()
+    def detect(self, enemy_group):       #stack overflow. Question: How to target something in pygame
 
-    def attack(self):
-        super().attack()
+        if self.stunned_cooldown == 0 or self.mainC.health !=0:
+            self.running = True
+            if self.rect.x > self.mainC.rect.x and self.direction == "right":
+                self.direction = "left"
+            elif self.rect.x < self.mainC.rect.x and self.direction == "left":
+                self.direction = "right"
+            self.x_vel = self.mainC.rect.x - self.rect.x
+            dist = math.hypot(self.x_vel)
+            if dist > 60:
+                self.x_vel = self.x_vel / dist 
+                self.rect.x += self.x_vel 
+            else:
+                self.running = False
+                self.attack(enemy_group)
+
+    def attack(self, enemy_group):
+        if self.mainC.health !=0:
+            super().attack(enemy_group)
 
     def check_collision(self, enemy_group):
         super().check_collision(enemy_group)
+
 
 
 
