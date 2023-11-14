53873139da4730889eaa3f6e76da97ca980f2dad
TheHiddenError <148292099+TheHiddenError@users.noreply.github.com>
2023-11-13 21:59:12 -0600

    Update EventHanlder.py
    Added functions to help create wave of enemies and multiple enemies at same time.

--- EventHanlder.py
+++ EventHanlder.py
@@ -1,12 +1,19 @@
 import pygame
+import random
 
 from Characters import*
 
 class Event:
-    def __init__(self, window,  mainC, enemy1 ):
+    def __init__(self, window,  mainC, mainGroup):
         self.window = window
         self.mainC = mainC
-        self.enemy1 = enemy1
+        self.x = 540        #to place the enemies on the ground
+        self.y = 365
+        self.active_wave = False    #to check if a wave has spawned
+        self.wave_number = 1        
+        self.mainGroup = mainGroup
+        self.enemyGroup = pygame.sprite.Group()        #sprite groups are to check mulitple collisions of the same type, like hitting multiple enemies
+        self.enemies = [] #this would hold the enemies
 
     def listen(self, theEvents):
         self.queueOfEvents = theEvents
@@ -15,9 +22,33 @@ class Event:
                 pygame.quit()
                 exit()
 
+    def waves(self):
+        if self.active_wave != True:        #if there is no wave
+            self.active_wave = True
+            i = 0
+            while i < self.wave_number:        #it creates the enemies
+                self.enemies.append(EnemyKnight(self.x + random.randrange(0,50, 10), self.y, 120, 180, self.mainC))
+                self.enemyGroup.add(self.enemies[i])    #adds to sprite group
+                i += 1
+            self.wave_number +=1    #increases wave number 
+
+    def check(self):
+        for enemy in self.enemies:    #if there is an enemy that is alive 
+            if enemy.health > 0:
+                return                 #it make sures that the another wave does not happen
+        self.active_wave = False
+
+    def handleEnemies(self):        #creates the interactions for enemies and draws on screen
+        for enemy in self.enemies:
+            enemy.loop()
+            enemy.detect(self.mainGroup)
+            enemy.draw(self.window)
+
+    def handle_attack(self):        #to handle the attack of main (to avoid issues with sprite group)
+        self.mainC.handle_attack(self.enemyGroup)
+
 
     def draw(self):
         self.mainC.draw(self.window)
-        self.enemy1.draw(self.window)
         pygame.display.update()
 
