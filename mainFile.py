505795f8c2e1ebc34ae890288b09ab0acf0aec45
TheHiddenError <148292099+TheHiddenError@users.noreply.github.com>
2023-11-27 09:53:19 -0600

    Update mainFile.py for starting and game over implementation.

--- mainFile.py
+++ mainFile.py
@@ -10,10 +10,7 @@ pygame.init()
 
 
 mainDisplay = pygame.display.set_mode((800, 600))
-
-
-
-
+   
 
 Clock = pygame.time.Clock()
 
@@ -21,11 +18,11 @@ Clock = pygame.time.Clock()
 def main(mainDisplay):
 
     fps = 60
-
+    
    
    
 
-    Neo = Necromancer(0, 500)                       #suppose to be main character. Lazy to rename
+    Neo = theMain(50, 500)                   
     boss = Boss(700, 200)
 
     handleEvents = Event(mainDisplay, boss, Neo)
@@ -33,35 +30,49 @@ def main(mainDisplay):
 
      
     while True:
-        handleEvents.draw_background()
-        Clock.tick(fps)
-
-
         handleEvents.listen(pygame.event.get())
+        if handleEvents.start == False and handleEvents.game_over == False:
+            handleEvents.startScreen()
 
+        elif handleEvents.game_over == False and handleEvents.start == True:
+            handleEvents.draw_background()  
+            Clock.tick(fps)
 
-        pygame.draw.rect(mainDisplay, (0,0,255), Neo.rect)
+            
 
+            # pygame.draw.rect(mainDisplay, (0,0,255), Neo.rect)
 
-
-        handleEvents.handleWaves()
-        handleEvents.spawnEnemeies()
-        handleEvents.scoreBoard()
-        handleEvents.check()
-        handleEvents.handleEnemies()
-
-        handleEvents.handle_cooldowns()
-
-        # handleEvents.handleBoss()
-        # handleEvents.goFaster()
-
+            handleEvents.handleWaves()
+            handleEvents.spawnEnemeies()    
+            
+            handleEvents.check()
+            handleEvents.handleEnemies()
         
-        
+            handleEvents.handle_cooldowns()
+            # handleEvents.movePowerup()
+            handleEvents.check_collision()
+            
 
-        Neo.loop()      #to get user input. Space for jump, d for ducking
+            handleEvents.scoreBoard()
+
+            # handleEvents.handleBoss()
+            handleEvents.draw()
+            handleEvents.goFaster()
+
+            
+            
+
+            Neo.loop()      #to get user input. Space for jump, d for ducking
+            Neo.update_sprite()
+
+        else:           #here is where the game ends, so insert end screen here
+            mainDisplay.fill((0,255,0))
+            handleEvents.showScore()
+      
 
         pygame.display.update()
 
 if __name__ == "__main__":      #only runs game through this file directly
     main(mainDisplay)
+
 
